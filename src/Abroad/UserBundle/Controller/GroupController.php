4442a257b81bdf68913cbfab572bbdbc7e3d2815
zzlajo <zuber.zlajo@gmail.com>
2015-07-05 21:14:02 +0200

    comment newAction

--- src/Abroad/UserBundle/Controller/GroupController.php
+++ src/Abroad/UserBundle/Controller/GroupController.php
@@ -36,69 +36,32 @@ class GroupController extends BaseController
     
     public function newAction(Request $request)
     {
-        /** @var $groupManager \FOS\UserBundle\Model\GroupManagerInterface */
-        $groupManager = $this->get('fos_user.group_manager');
-        /** @var $formFactory \FOS\UserBundle\Form\Factory\FactoryInterface */
-        $formFactory = $this->get('fos_user.group.form.factory');
-
-        $group = $groupManager->createGroup('');
-	
-
-        $form = $formFactory->createForm();
-        $form->setData($group);
-
-	$form->handleRequest($request);
-        if ($form->isValid()) {
-	    $gName = $group->getName();
-	    $user1 = $this->getUser()->getId();
-	    var_dump($user1);
-	    $gId = $this->findGroupBy('name', $gName);
-	    var_dump($gId);
-	    die;
-	    
-	}
+//        /** @var $groupManager \FOS\UserBundle\Model\GroupManagerInterface */
+//        $groupManager = $this->get('fos_user.group_manager');
+//        /** @var $formFactory \FOS\UserBundle\Form\Factory\FactoryInterface */
+//        $formFactory = $this->get('fos_user.group.form.factory');
+//
+//        $group = $groupManager->createGroup('');
+//	
+//
+//        $form = $formFactory->createForm();
+//        $form->setData($group);
+//
+//	$form->handleRequest($request);
+//        if ($form->isValid()) {
+//	    $gName = $group->getName();
+//	    $user1 = $this->getUser()->getId();
+//	    var_dump($user1);
+//	    $gId = $this->findGroupBy('name', $gName);
+//	    var_dump($gId);
+//	    die;
+//	    
+//	}
 	
 
 //$groupEntity = $this->getGroup($groupId);
     }
-////    public function createAction($blog_id) {
-////	
-////	$blog = $this->getBlog($blog_id);
-////	
-////	$comment = new Comment();
-////	$comment->setBlog($blog);
-////	$request = $this->getRequest();
-////	$form = $this->createForm(new CommentType(), $comment);
-////	$form->bind($request);
-////	
-////
-////        if ($form->isValid()) {
-////            // TODO: Persist the comment entity
-////	    $em = $this->getDoctrine()->getEntityManager();
-////	    $em->persist($comment);
-////	    $em->flush();
-////
-//////            return $this->redirect($this->generateUrl('AbroadBlogBundle_blog_show', array(
-//////                'id' => $comment->getBlog()->getId())) .
-//////                '#comment-' . $comment->getId()
-//////            );
-////	    return $this->redirect($this->generateUrl('AbroadBlogBundle_blog_show', array(
-////		'id'    => $comment->getBlog()->getId(),
-////		'slug'  => $comment->getBlog()->getSlug())) .
-////		'#comment-' . $comment->getId()
-////	    );
-////        }
-////
-////        return $this->render('AbroadBlogBundle:Comment:create.html.twig', array(
-////            'comment' => $comment,
-////            'form'    => $form->createView()
-////        ));
-////	
-////    }
-//    
-//	
-//	
-//    }
+
 
     
 }
